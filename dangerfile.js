0e28672f125db2a0e1d7320e6f0d94a608eaed08
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-12-28 12:00:19 -0600

    apply stash

--- dangerfile.js
+++ dangerfile.js
@@ -1,26 +1,53 @@
-// // eslint-disable-next-line import/no-extraneous-dependencies, no-unused-vars, object-curly-newline
-// import { danger, warn, fail, message } from 'danger';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { danger, fail } from 'danger';
 
-// const modifiedChangelog = danger.git.modified_files.filter((filePath) => {
-//   const srcFilePattern = /CHANGELOG.md/i;
-//   return srcFilePattern.test(filePath);
-// });
+const hasCHANGELOGChanges = danger.git.modified_files.some((filePath) => {
+  const srcFilePattern = /CHANGELOG.md/i;
+  return srcFilePattern.test(filePath);
+});
 
-// const modifiedConfigFiles = danger.git.modified_files.filter((filePath) => {
-//   const scriptFilePattern = /eslint.config.js/i;
-//   return scriptFilePattern.test(filePath);
-// });
+const hasModifiedRulesFiles = danger.git.modified_files.some((filePath) => {
+  const srcFilePattern = /lib\/rules/i;
+  return srcFilePattern.test(filePath);
+});
 
-// const hasCHANGELOGChanges = modifiedChangelog.length > 0;
-// const hasModifiedConfigFiles = modifiedConfigFiles.length > 0;
+const addedRules = danger.git.created_files.filter((filePath) => {
+  const srcFilePattern = /lib\/rules/i;
+  return srcFilePattern.test(filePath);
+});
 
-// // Fail if there are src code changes without a CHANGELOG update
-// if (hasModifiedConfigFiles && !hasCHANGELOGChanges) {
-//   fail('Please include a CHANGELOG entry with this PR.');
-// }
+const hasNewRules = addedRules.length > 0;
 
-// // Warn when there is a big PR
-// const bigPRThreshold = 1000;
-// if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-//   warn(':exclamation: Big PR. Consider breaking this into smaller PRs if applicaple');
-// }
+// Fail if there are rule additions or changes without a CHANGELOG update
+if ((hasModifiedRulesFiles || hasNewRules) && !hasCHANGELOGChanges) {
+  fail('Please include a CHANGELOG entry with this PR.');
+}
+
+if (hasNewRules) {
+  const addedDocs = danger.git.created_files.filter((filePath) => {
+    const srcFilePattern = /docs\/rules/;
+    return srcFilePattern.test(filePath);
+  });
+
+  const hasDocForEachNewRule = addedRules.every(ruleFile => (
+    addedDocs.every((docFile) => {
+      const ruleName = ruleFile.replace(/lib\/rules\//, '').replace('.js', '');
+      return docFile.includes(ruleName);
+    })
+  ));
+
+  const hasREADMEChanges = danger.git.created_files.some((filePath) => {
+    const srcFilePattern = /README.md/i;
+    return srcFilePattern.test(filePath);
+  });
+
+  // Fail if a new rule is added without its corresponding documentent changes
+  if (hasNewRules && !hasDocForEachNewRule) {
+    fail('Please include the corresponding rule documentation.');
+  }
+
+  // Fail if the new rule(s) were not added to the README
+  if (hasNewRules && !hasREADMEChanges) {
+    fail('Please add the new rule(s) to the rule list in the README.');
+  }
+}
